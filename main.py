d192772af79d163d8cdc5b67e62aebb3a327c24f
patriciaultracognia <patricia.ultracognia@gmail.com>
2026-04-25 03:20:14 -0300

    Correção no main

--- main.py
+++ main.py
@@ -20,21 +20,21 @@ app = FastAPI()
 
 app.add_middleware(GZipMiddleware, minimum_size=1000) #compacta acima de 1kb
 
-@app.get("/")
-def home():
-    return {"status": "API Online"}
-
 @app.middleware("http")
 async def validar_acesso(request: Request, call_next):
     api_key = request.headers.get("x-api-key");
     API_KEY = os.getenv("API_KEY", "")
 
     if api_key != API_KEY:
-        logger.warning((f"Headers recebidos: {dict(request.headers)}")
-        logger.warning((f"Chaves: {api_key} - {API_KEY}")
+        logger.warning(f"Headers recebidos: {dict(request.headers)}")
+        logger.warning(f"Chaves: {api_key} - {API_KEY}")
         return JSONResponse(status_code=403, content={"detail": "Não autorizado"})
 
     return await call_next(request)
 
 # Registra as rotas de chat
 app.include_router(chat_router)
+
+@app.get("/")
+def home():
+    return {"status": "API Online"}
